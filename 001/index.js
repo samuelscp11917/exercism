688e8b2c2b6fe1be2258bf92ed6328f66131f5d3
samuelscp11917 <samuelscp11917@gmail.com>
2024-03-19 16:33:27 -0500

    exercise 001

---
+++ 001/index.js
@@ -0,0 +1,5 @@
+import { hello } from './hello-world.js';
+
+const result = hello(); // Llama a la función hello() y guarda su resultado en una variable
+console.log(result); // Imprime el resultado en la consola
+console.log('Debug message'); // Esto imprimirá "Debug message" en la consola
